f3d86bf39f13af94417b4daf39ddbaa3624801c8
Ricky Tran <rtran58@stanford.edu>
2017-03-23 15:03:12 -0700

    2 - Delegate channel name using props

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 class Channel extends React.Component{
   render(){
     return (
-      <li>Channel name</li>
+      <li>{this.props.name}</li>
     )
   }
 }
 
-ReactDOM.render(<Channel />, document.getElementById('app'));
+ReactDOM.render(<Channel name='Hardware Support'/>, document.getElementById('app'));

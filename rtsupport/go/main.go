fe6ac58cd8c756f55cd1fcdd716af4be026b6444
Ricky Tran <rtran58@stanford.edu>
2017-03-25 22:27:31 -0700

    15 - Use goroutines for concurrent actions

--- rtsupport/go/main.go
+++ rtsupport/go/main.go
@@ -4,8 +4,21 @@ import (
 	"fmt"
 	"net/http"
 
+	"time"
+
 	"github.com/gorilla/websocket"
+	"github.com/mitchellh/mapstructure"
 )
+
+type Message struct {
+	Name string      `json:"name"`
+	Data interface{} `json:"data"`
+}
+
+type Channel struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -27,15 +40,58 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	for {
-		msgType, msg, err := socket.ReadMessage()
-		if err != nil {
+		// msgType, msg, err := socket.ReadMessage()
+		// if err != nil {
+		// 	fmt.Println(err)
+		// 	return
+		// }
+		var inMessage Message
+		var outMessage Message
+		if err := socket.ReadJSON(&inMessage); err != nil {
 			fmt.Println(err)
-			return
+			break
 		}
-		fmt.Println(string(msg))
-		if err = socket.WriteMessage(msgType, msg); err != nil {
-			fmt.Println(err)
-			return
+		fmt.Printf("%#v\n", inMessage)
+		switch inMessage.Name {
+		case "channel add":
+			err := addChannel(inMessage.Data)
+			if err != nil {
+				outMessage = Message{"error", err}
+				if err := socket.WriteJSON(outMessage); err != nil {
+					fmt.Println(err)
+					break
+				}
+			}
+		case "channel subscribe":
+			go subscribeChannel(socket)
 		}
+
+		// fmt.Println(string(msg))
+		// if err = socket.WriteMessage(msgType, msg); err != nil {
+		// 	fmt.Println(err)
+		// 	return
+		// }
 	}
+}
+
+func addChannel(data interface{}) error {
+	var channel Channel
+	err := mapstructure.Decode(data, &channel)
+	if err != nil {
+		return err
+	}
+	channel.Id = "1"
+	fmt.Println("added channel")
+	return nil
+}
+
+func subscribeChannel(socket *websocket.Conn) {
+	for {
+		time.Sleep(time.Second * 1)
+		message := Message{"channel add",
+			Channel{"1", "Software Support"}}
+		socket.WriteJSON(message)
+		fmt.Println("send new channel")
+	}
+	// TODO: rethinkDB Query / changefeed
 }
